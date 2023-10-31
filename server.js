11cc1c423e510810d51f1c13ebe63e8f72b2701f
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-10-31 08:09:40 -0300

    base

---
+++ server.js
@@ -0,0 +1,50 @@
+import { fastify } from 'fastify';
+import { DatabaseMemory } from './database-memory.js';
+
+const server = fastify();
+
+const database = new DatabaseMemory();
+
+
+
+server.post('/videos', (request,reply) => {
+    const {title, description, duration} = request.body;
+
+    database.create({
+        title,
+        description,
+        duration,
+    });
+
+    return reply.status(201).send();
+})
+
+server.get('/videos', (request,reply) => {
+    const videos = database.list();
+    return videos;
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+    const {title, description, duration} = request.body;
+
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    });
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+
+    database.delete(videoId);
+
+    return reply.status(204).send();
+})
+
+server.listen({
+    port: 3333,
+});
